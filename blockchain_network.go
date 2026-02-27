c8969dd157b2fdf5b35885a23d5f7f56ffb6f270
Oleksii Zhylko <alexey.zhylko@crosspay.net>
2026-02-27 14:15:34 +0200

    feat(CPD-7146): Add BlockchainNetwok

---
+++ blockchain_network.go
@@ -0,0 +1,48 @@
+package currency
+
+type BlockchainNetwork string
+
+const (
+	Erc20        BlockchainNetwork = "erc20"
+	Trc20        BlockchainNetwork = "trc20"
+	Bep20        BlockchainNetwork = "bep20"
+	Bep2         BlockchainNetwork = "bep2"
+	Sol          BlockchainNetwork = "sol"
+	Base         BlockchainNetwork = "base"
+	Optimism     BlockchainNetwork = "optimism"
+	Arbitrum     BlockchainNetwork = "arbitrum"
+	Stellar      BlockchainNetwork = "stellar"
+	Cardano      BlockchainNetwork = "cardano"
+	Eip155_10    BlockchainNetwork = "eip155:10"
+	Eip155_137   BlockchainNetwork = "eip155:137"
+	Eip155_8453  BlockchainNetwork = "eip155:8453"
+	Eip155_42161 BlockchainNetwork = "eip155:42161"
+)
+
+func (n BlockchainNetwork) isTagOrMemoRequired() bool {
+	switch n {
+	case Stellar,
+		Cardano:
+		return true
+	}
+	return false
+}
+
+func GetAllBlockchainNetwork() []BlockchainNetwork {
+	return []BlockchainNetwork{
+		Erc20,
+		Trc20,
+		Bep20,
+		Bep2,
+		Sol,
+		Base,
+		Optimism,
+		Arbitrum,
+		Stellar,
+		Cardano,
+		Eip155_10,
+		Eip155_137,
+		Eip155_8453,
+		Eip155_42161,
+	}
+}
