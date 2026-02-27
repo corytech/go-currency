616bf91ae559e0353f3ca73f2f619f9859613f9e
OleksiiZhylko <172418466+OleksiiZhylko@users.noreply.github.com>
2026-02-27 14:27:24 +0200

    feat(CPD-7146): Add BlockchainNetwok (#9)

---
+++ blockchain_network_test.go
@@ -0,0 +1,57 @@
+package currency
+
+import (
+	"slices"
+	"testing"
+)
+
+var AllBlockchainNetwork = []string{
+	"erc20",
+	"trc20",
+	"bep20",
+	"bep2",
+	"sol",
+	"base",
+	"optimism",
+	"arbitrum",
+	"stellar",
+	"cardano",
+	"eip155:10",
+	"eip155:137",
+	"eip155:8453",
+	"eip155:42161",
+}
+
+var BlockChainTagMemoRequires = map[string]bool{
+	"erc20":        false,
+	"trc20":        false,
+	"bep20":        false,
+	"bep2":         false,
+	"sol":          false,
+	"base":         false,
+	"optimism":     false,
+	"arbitrum":     false,
+	"stellar":      true,
+	"cardano":      true,
+	"eip155:10":    false,
+	"eip155:137":   false,
+	"eip155:8453":  false,
+	"eip155:42161": false,
+}
+
+func TestGetAllBlockchainNetwork(t *testing.T) {
+	blockchainNetworks := GetAllBlockchainNetwork()
+	for _, blockchainNetwork := range AllBlockchainNetwork {
+		if !slices.Contains(blockchainNetworks, BlockchainNetwork(blockchainNetwork)) {
+			t.Fatalf("Expected '%v' in list '%v'", blockchainNetwork, blockchainNetworks)
+		}
+	}
+}
+
+func TestIsTagOrMemoRequired(t *testing.T) {
+	for blockchainNetwork, isMemoRequired := range BlockChainTagMemoRequires {
+		if BlockchainNetwork(blockchainNetwork).isTagOrMemoRequired() != isMemoRequired {
+			t.Fatalf("Expected '%v' isTagOrMemoRequired '%v' get '%v'", blockchainNetwork, isMemoRequired, BlockchainNetwork(blockchainNetwork).isTagOrMemoRequired())
+		}
+	}
+}
