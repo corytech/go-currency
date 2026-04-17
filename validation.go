d8ed8341dbf70204ed3498fb54ff6ec1dc7fdd99
Oleksii Zhylko <alexey.zhylko@crosspay.net>
2026-04-17 15:44:17 +0300

    feat(CPD-7146): Add validator BlockchainNetwok

--- validation.go
+++ validation.go
@@ -32,3 +32,30 @@ func isCorrectCurrency(fl validator.FieldLevel) bool {
 
 	return Currency(v).IsValid()
 }
+
+func RegisterBlockchainNetworkValidator(v *validator.Validate, t *ut.Translator) {
+	_ = v.RegisterValidation("IsCorrectBlockchainNetwork", isCorrectBlockchainNetwork)
+	_ = v.RegisterTranslation(
+		"IsCorrectBlockchainNetwork",
+		*t,
+		func(trans ut.Translator) error {
+			if err := trans.Add("IsCorrectBlockchainNetwork", "{0} is not correct blockchainNetwork", false); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		func(trans ut.Translator, fe validator.FieldError) string {
+			msg, err := trans.T(fe.Tag(), fe.Field())
+			if err != nil {
+				return ""
+			}
+
+			return msg
+		})
+}
+
+func isCorrectBlockchainNetwork(fl validator.FieldLevel) bool {
+	v := fl.Field().String()
+	return BlockchainNetwork(v).IsValid()
+}
